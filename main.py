2fb085a0bede127f97c5cbfe00c0c01b3518f54e
enzofaceroli <nikkzin@gmail.com>
2026-01-19 17:45:45 -0300

    improve colab support

--- main.py
+++ main.py
@@ -4,13 +4,29 @@ import torch.optim as optim
 
 from data_loader.cifar10 import get_cifar10_loaders
 
+import argparse
 from tests.vgg16_tests import VGG16_TESTS 
 from tests.vgg16_manual_tests import VGG16_MANUAL_TESTS 
 from tests.resnet50_tests import RESNET50_TESTS 
 from tests.densenet121_tests import DENSENET121_TESTS 
 
-# ESCOLHER TESTES AQUI
-TESTS = VGG16_TESTS
+# Isso é para executar no Colab
+TESTS_MAP = {
+    "vgg16": VGG16_TESTS,
+    "vgg16_manual": VGG16_MANUAL_TESTS,
+    "resnet50": RESNET50_TESTS,
+    "densenet121": DENSENET121_TESTS
+}
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--arch",
+        type=str,
+        required=True,
+        choices=TESTS_MAP.keys()
+    )
+    return parser.parse_args()
 
 from models.resnet50_transfer import build_resnet50
 from models.vgg16_transfer import build_vgg16_transfer
@@ -52,6 +68,9 @@ def get_model(exp_config, num_classes):
 
 
 def main():
+    args = parse_args()
+    TESTS = TESTS_MAP[args.arch]
+    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Dispositivo: {device}")
 
