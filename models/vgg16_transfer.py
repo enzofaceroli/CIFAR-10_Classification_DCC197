29e9979e24c225d56dc62394ac02562e83b142ea
enzofaceroli <nikkzin@gmail.com>
2026-01-19 16:36:13 -0300

    bugfix

--- models/vgg16_transfer.py
+++ models/vgg16_transfer.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from torchvision.models import vgg16, VGG16_Weights
 
 def build_vgg16_transfer(num_classes = 10, freeze_backbone = True):
-    model = vgg16(weights = VGG16_Weights)
+    model = vgg16(weights = VGG16_Weights.DEFAULT)
     
     if freeze_backbone:
         for param in model.parameters():
