c4b69a66194ce5e24bd8c2538a5db525fc4b7b0d
enzofaceroli <nikkzin@gmail.com>
2026-01-19 16:35:03 -0300

    bugfix

--- models/vgg16_transfer.py
+++ models/vgg16_transfer.py
@@ -11,7 +11,7 @@ def build_vgg16_transfer(num_classes = 10, freeze_backbone = True):
     in_features = model.classifier[-1].in_features
     model.classifier[-1] = nn.Linear(in_features, num_classes)
     
-    for param in model.fc.parameters():
+    for param in model.classifier.parameters():
         param.requires_grad = True
         
     return model
